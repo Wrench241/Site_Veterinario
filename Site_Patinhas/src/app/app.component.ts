7ae71ec0b0b6df37403f9fa5dd34af942a8f7eb2
wrench241 <nickalex241@gmail.com>
2023-08-23 16:48:31 -0300

    finalizando list-item e style

--- Site_Patinhas/src/app/app.component.ts
+++ Site_Patinhas/src/app/app.component.ts
@@ -14,4 +14,31 @@ showFiller: any;
     window.location.href='https://www.instagram.com';
   }
 
+  list = [
+    {
+      number: '1',
+      name: 'Redes sociais',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      number: '2',
+      name: 'Localização',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      number: '3',
+      name: 'Catálogo',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      number: '4',
+      name: 'Contato',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      number: '5',
+      name: 'Sobre',
+      icon: 'fa-solid fa-house',
+    },
+  ];
 }
